27e04efd3f298f20d980f5eb6997736830320a53
awebow <bigmsh11@gmail.com>
2019-04-02 18:21:02 +0900

    Implement error handling

---
+++ main/error.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"github.com/ant0ine/go-json-rest/rest"
+)
+
+type ApiError int
+
+func (err ApiError) Error() string {
+	switch err {
+	case 404:
+		return "Resource not found"
+	case 409:
+		return "Resource conflicts"
+	default:
+		return "Unknown error"
+	}
+}
+
+const (
+	ResourceNotFound  ApiError = 404
+	ResourceConflicts ApiError = 409
+)
+
+func ResponseError(w rest.ResponseWriter, err error) {
+	aerr, ok := err.(ApiError)
+	if !ok {
+		aerr = 500
+	}
+
+	w.WriteHeader(int(aerr))
+	w.WriteJson(map[string]interface{}{
+		"error": aerr.Error(),
+	})
+}
